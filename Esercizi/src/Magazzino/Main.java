4f69b401a80b8954a22ae664d865d9452ab8e9ee
Cappalunga Stefano <cappalunga.stefano@osdbsesto>
2026-04-24 14:22:33 +0200

    [ADD] Aggiunto Main

--- Esercizi/src/Magazzino/Main.java
+++ Esercizi/src/Magazzino/Main.java
@@ -1,7 +1,25 @@
 package Magazzino;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Magazzino magazzino = new Magazzino();
 
+        Operaio o1 = new Operaio("Operaio1", magazzino);
+        Operaio o2 = new Operaio("Operaio2", magazzino);
+        Operaio o3 = new Operaio("Operaio3", magazzino);
+
+        Corriere corriere = new Corriere(magazzino);
+
+        o1.start();
+        o2.start();
+        o3.start();
+        corriere.start();
+
+        o1.join();
+        o2.join();
+        o3.join();
+        corriere.join();
+
+        System.out.println("Simulazione terminata.");
     }
 }
