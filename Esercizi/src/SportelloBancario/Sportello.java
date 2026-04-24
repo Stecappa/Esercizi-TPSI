7f58f2079717214ccd7692bd421370bbc0bb15d3
Cappalunga Stefano <cappalunga.stefano@osdbsesto>
2026-04-24 13:32:28 +0200

    [ADD] Aggiunta classe sportello bancario

---
+++ Esercizi/src/SportelloBancario/Sportello.java
@@ -0,0 +1,33 @@
+package SportelloBancario;
+
+public class Sportello {
+
+    private int cassieriLiberi = 2;
+
+    public synchronized boolean accedi(long timeout) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        long tempoRimanente = timeout;
+
+        while (cassieriLiberi == 0) {
+            if (tempoRimanente <= 0) {
+                return false;
+            }
+
+            wait(tempoRimanente);
+
+            long ora = System.currentTimeMillis();
+            tempoRimanente = timeout - (ora - start);
+        }
+
+
+        cassieriLiberi--;
+        return true;
+    }
+
+    public synchronized void lascia() {
+        cassieriLiberi++;
+        notifyAll();
+    }
+
+    
+}
